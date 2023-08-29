f9827b95dda18afbeba18efd8847ff1e37790725
Christian <hernandezchristian94@gmail.com>
2023-08-29 12:21:15 -0700

    added components and an apout section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'; // Import Provider
+import { store } from './redux/store'; // Import your Redux store
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
